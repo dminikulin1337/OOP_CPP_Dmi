bc32c68111d69bd6dac8ecbb96fe9594be6a9440
dminikulin1337 <dminikulin2003@gmail.com>
2020-05-20 21:26:23 +0300

    Concepts of OOP

--- Introduction to OOP/main.cpp
+++ Introduction to OOP/main.cpp
@@ -1,19 +1,38 @@
 #include<iostream>
 using namespace std;
 
-class Point
+class Point //Main class with encapsulation
 {
 	double x;
 	double y;
+public:
+	double get_x() const { return x; }
+	double get_y() const { return y; }
+	void set_x(double x) { this->x = x; }
+	void set_y(double y) { this->y = y; }
+};
+
+class Point3D :public Point //Inheritated class frae Point
+{
+	double z;
+public:
+	double get_z() const { return z; }
+	void set_z(double z) { this->z = z; }
 };
 
 void main() 
 {
 	Point A;
-	A.x = 2;
-	A.y = 3;	
-	cout << A.x << "\t" << A.y << endl;
+	A.set_x(2);
+	A.set_y(3);	
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 
 	Point* pA = &A;
-	cout << pA->x << "\t" << pA->y << endl;
+	cout << pA->get_x() << "\t" << pA->get_y() << endl;
+
+	Point3D B;
+	B.set_x(2);
+	B.set_y(3);
+	B.set_z(4);
+	
 }
